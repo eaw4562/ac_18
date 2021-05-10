759cc2cfb92e9865e95dc61430f3b3f9f0dd256d
eaw4562 <eaw4562@naver.com>
2021-05-11 00:23:04 +0900

    Initial commit

---
+++ app/src/main/java/com/example/senierproject/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.senierproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getHashKey();
+    }
+
+    private void getHashKey() {
+
+    }
+}
